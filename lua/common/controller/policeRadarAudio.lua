c203fdcb793d675d8443f8b1b5034676f15e69d6
Angelo Matteo <angmatt90@gmail.com>
2024-12-21 16:26:02 -0800

    fix audio sources

--- lua/common/controller/policeRadarAudio.lua
+++ lua/common/controller/policeRadarAudio.lua
@@ -4,8 +4,10 @@ local radar_doppler_sfx = nil
 
 local voice_sfx_table = {}
 
-local radar_doppler_sfx_src = 'art/sound/550hz.wav'
+local radar_doppler_sfx_src = 'art/sound/police_radar_angelo234/550hz.wav'
 local radar_doppler_sfx_src_speed = 13.4112 -- 30 mph
+
+local select_sfx_src = 'art/sound/police_radar_angelo234/select.wav'
 
 local radar_doppler_volume = 2
 
@@ -22,8 +24,7 @@ local locked_speed_voice_vel = nil
 
 local function init()
   radar_doppler_sfx = obj:createSFXSource(radar_doppler_sfx_src, 'AudioDefaultLoop3D', '', 1)
-
-  obj:createSFXSource('art/sound/speech_police_radar_angelo234/select.wav', 'AudioGui', 'radar_select', 1)
+  obj:createSFXSource(select_sfx_src, 'AudioGui', 'radar_select', 1)
 
   --Voices (credits: https://voicemaker.in/)
   voice_sfx_table["front"] = {sfx_name = "radar_voice_front", length = 0.45}
@@ -35,7 +36,7 @@ local function init()
   voice_sfx_table["away"] = {sfx_name = "radar_voice_away", length = 0.45}
 
   for key, data in pairs(voice_sfx_table) do
-    obj:createSFXSource('art/sound/speech_police_radar_angelo234/speech/' .. key .. '.wav', 'AudioGui', data.sfx_name, 1)
+    obj:createSFXSource('art/sound/police_radar_angelo234/speech/' .. key .. '.wav', 'AudioGui', data.sfx_name, 1)
   end
 end
 
